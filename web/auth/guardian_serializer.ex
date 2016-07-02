ebc7ea5bac783fbab1420f40ec4cdce6b6963597
Jeff <200006506@qq.com>
2016-07-03 00:42:35 +0800

    up

--- web/auth/guardian_serializer.ex
+++ web/auth/guardian_serializer.ex
@@ -7,12 +7,6 @@ defmodule PhoenixChina.GuardianSerializer do
   def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
   def for_token(_), do: { :error, "Unknown resource type" }
 
-  def from_token("User:" <> id), do: { :ok, Repo.get(User, String.to_integer(id)) }
+  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
   def from_token(_), do: { :error, "Unknown resource type" }
-
-  def unauthenticated(conn, _params) do
-    conn
-    |> Phoenix.Controller.put_flash(:error, "Authentication required")
-    |> Phoenix.Controller.redirect(to: "/signin")
-  end
 end
