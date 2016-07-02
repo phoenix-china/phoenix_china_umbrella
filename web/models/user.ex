c48cef517af96ec5b1b871affad2f93c6203fbad
Jeff <200006506@qq.com>
2016-07-02 20:58:31 +0800

    up model

--- web/models/user.ex
+++ web/models/user.ex
@@ -37,22 +37,6 @@ defmodule PhoenixChina.User do
     |> validate_length(:password, min: 6, max: 128)
   end
 
-  @doc """
-  验证用户邮箱是否存在
-  """
-  def validate_email(changeset) do
-    if is_nil(changeset.changes.email) do
-      changeset
-      |> add_error(:email, "用户不存在")
-    else
-      changeset
-    end
-  end
-
-  def validate_password(changeset) do
-    changeset
-  end
-
   def put_password_hash(changeset) do
     password_hash = changeset.changes.password
     |> Comeonin.Bcrypt.hashpwsalt
