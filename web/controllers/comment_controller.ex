2c3c7ccb8b5ba600dd542a2c4d7d55cd9e1a80b8
Jeff <200006506@qq.com>
2016-07-03 15:04:51 +0800

    add verify comment owner

--- web/controllers/comment_controller.ex
+++ web/controllers/comment_controller.ex
@@ -2,7 +2,7 @@ defmodule PhoenixChina.CommentController do
   use PhoenixChina.Web, :controller
 
   alias PhoenixChina.Comment
-  import PhoenixChina.ViewHelpers, only: [logged_in?: 1, current_user: 1]
+  import PhoenixChina.ViewHelpers, only: [current_user: 1]
 
   plug Guardian.Plug.EnsureAuthenticated, [handler: PhoenixChina.GuardianHandler]
   when action in [:new, :create, :edit, :update, :delete]
@@ -32,39 +32,58 @@ defmodule PhoenixChina.CommentController do
     current_user = current_user(conn)
     comment = Repo.get!(Comment, id)
 
-    if comment.user_id != current_user.id do
-      conn
-      |> put_flash(:info, "不是自己的评论，不允许编辑！")
-      |> redirect(to: post_path(conn, :show, post_id))
-    end
+    case comment.user_id != current_user.id do
+      false ->
+        conn
+        |> put_flash(:info, "不是自己的评论，不允许编辑！")
+        |> redirect(to: post_path(conn, :show, post_id))
 
-    changeset = Comment.changeset(comment)
-    render(conn, "edit.html", post_id: post_id, comment: comment, changeset: changeset)
+      true ->
+        changeset = Comment.changeset(comment)
+        render(conn, "edit.html", post_id: post_id, comment: comment, changeset: changeset)
+    end
   end
 
   def update(conn, %{"post_id" => post_id, "id" => id, "comment" => comment_params}) do
+    current_user = current_user(conn)
     comment = Repo.get!(Comment, id)
-    changeset = Comment.changeset(comment, comment_params)
 
-    case Repo.update(changeset) do
-      {:ok, comment} ->
+    case comment.user_id != current_user.id do
+      false ->
         conn
-        |> put_flash(:info, "Comment updated successfully.")
+        |> put_flash(:info, "不是自己的评论，不允许编辑！")
         |> redirect(to: post_path(conn, :show, post_id))
-      {:error, changeset} ->
-        render(conn, "edit.html", comment: comment, changeset: changeset)
+
+      true ->
+        changeset = Comment.changeset(comment, comment_params)
+
+        case Repo.update(changeset) do
+          {:ok, _comment} ->
+            conn
+            |> put_flash(:info, "Comment updated successfully.")
+            |> redirect(to: post_path(conn, :show, post_id))
+          {:error, changeset} ->
+            render(conn, "edit.html", comment: comment, changeset: changeset)
+        end
     end
   end
 
   def delete(conn, %{"post_id" => post_id, "id" => id}) do
+    current_user = current_user(conn)
     comment = Repo.get!(Comment, id)
 
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(comment)
+    case comment.user_id != current_user.id do
+      false ->
+        conn
+        |> put_flash(:info, "不是自己的评论，不允许删除！")
+        |> redirect(to: post_path(conn, :show, post_id))
 
-    conn
-    |> put_flash(:info, "Comment deleted successfully.")
-    |> redirect(to: post_path(conn, :show, post_id))
+      true ->
+        Repo.delete!(comment)
+
+        conn
+        |> put_flash(:info, "评论删除成功！")
+        |> redirect(to: post_path(conn, :show, post_id))
+    end
   end
 end
--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -2,7 +2,7 @@ defmodule PhoenixChina.PostController do
   use PhoenixChina.Web, :controller
 
   alias PhoenixChina.Post
-  import PhoenixChina.ViewHelpers, only: [logged_in?: 1, current_user: 1]
+  import PhoenixChina.ViewHelpers, only: [current_user: 1]
 
   plug Guardian.Plug.EnsureAuthenticated, [handler: PhoenixChina.GuardianHandler]
   when action in [:new, :create, :edit, :update, :delete]
