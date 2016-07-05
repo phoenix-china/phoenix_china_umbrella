1859768156ee40fb9352ef342b03d28ea4e73efe
Jeff <200006506@qq.com>
2016-07-05 21:46:19 +0800

    邮件发送初始化

--- lib/phoenix_china/mailer.ex
+++ lib/phoenix_china/mailer.ex
@@ -3,21 +3,20 @@ defmodule PhoenixChina.Mailer do
     domain: Application.get_env(:phoenix_china, :mailgun_domain),
     key: Application.get_env(:phoenix_china, :mailgun_key)
 
+  import Phoenix.View, only: [render: 3]
+  alias PhoenixChina.PageView
+
   @from "Phoenix中文社区 <mail@phoenix-china.org>"
 
   def send_confirmation_email(user) do
-     send_email to: user.email,
-                from: @from,
-                subject: "确认邮件",
-                text: "确认邮件",
-                html: "确认邮件"
+    text = render(PageView, "confirmation_email.txt", user: user)
+    html = render(PageView, "confirmation_email.html", user: user)
+    send_email to: user.email, from: @from, subject: "确认邮件", text: text, html: html
   end
 
   def send_reset_password_email(user) do
-    send_email to: user.email,
-               from: @from,
-               subject: "重置密码",
-               text: "重置密码",
-               html: "重置密码"
+    text = render(PageView, "reset_password_email.txt", user: user)
+    html = render(PageView, "reset_password_email.html", user: user)
+    send_email to: user.email, from: @from, subject: "重置密码", text: text, html: html
   end
 end
