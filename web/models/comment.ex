b32e92ebc7811bf1ef4582ca5f39097c167c4574
Jeff <200006506@qq.com>
2016-07-06 13:17:43 +0800

    评论的markdown处理

--- web/models/comment.ex
+++ web/models/comment.ex
@@ -16,5 +16,19 @@ defmodule PhoenixChina.Comment do
     struct
     |> cast(params, [:content])
     |> validate_required([:content])
+    |> strip_unsafe_content(params)
+  end
+
+  defp strip_unsafe_content(struct, %{"content" => nil}) do
+    struct
+  end
+
+  defp strip_unsafe_content(struct, %{"content" => body}) do
+    {:safe, clean_body} = Phoenix.HTML.html_escape(body)
+    struct |> put_change(:content, clean_body)
+  end
+
+  defp strip_unsafe_content(struct, _) do
+    struct
   end
 end
