3df3870a083bbac98fc8504c7769e034481cbd0a
Jeff <200006506@qq.com>
2016-07-07 14:27:16 +0800

    调整富文本编辑器，开启自动保存

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -38,6 +38,7 @@ defmodule PhoenixChina.PostController do
   end
 
   def show(conn, %{"id" => id}) do
+    IO.inspect conn
     post = (from Post, where: [id: ^id], preload: [:user, :latest_comment, latest_comment: :user]) |> first |> Repo.one!
     comments = (from Comment, where: [post_id: ^id], order_by: [asc: :inserted_at], preload: [:user])
     |> Repo.all
