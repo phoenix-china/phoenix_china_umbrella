bc4e61ee1fc06ca16a8707c9c9609fcc55b57e52
Jeff <200006506@qq.com>
2016-07-10 10:47:35 +0800

    更新brunch配置文件

--- brunch-config.js
+++ brunch-config.js
@@ -35,8 +35,7 @@ exports.config = {
     // By default, we set this to "/web/static/assets". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
     assets: [
-      /^(web\/static\/assets)/,
-      /^(node_modules\/font-awesome)/
+      /^(web\/static\/assets)/
     ]
   },
 
@@ -45,12 +44,7 @@ exports.config = {
     // Dependencies and current project directories to watch
     watched: [
       "web/static",
-      "test/static",
-      'node_modules/font-awesome/fonts/fontawesome-webfont.eot',
-      'node_modules/font-awesome/fonts/fontawesome-webfont.svg',
-      'node_modules/font-awesome/fonts/fontawesome-webfont.ttf',
-      'node_modules/font-awesome/fonts/fontawesome-webfont.woff',
-      'node_modules/font-awesome/fonts/fontawesome-webfont.woff2'
+      "test/static"
     ],
 
     // Where to compile files to
