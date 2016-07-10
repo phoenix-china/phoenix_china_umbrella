0bad772a225ac3ab3f82787d37c046c155f2f5ed
Jeff <200006506@qq.com>
2016-07-10 14:39:07 +0800

    添加bootstrap脚本

--- brunch-config.js
+++ brunch-config.js
@@ -2,7 +2,7 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      joinTo: "js/app.js",
 
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
@@ -12,12 +12,12 @@ exports.config = {
       // }
       //
       // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "web/static/vendor/js/jquery-2.1.1.js",
-      //     "web/static/vendor/js/bootstrap.min.js"
-      //   ]
-      // }
+      order: {
+        before: [
+          "web/static/vendor/js/jquery.min.js",
+          "web/static/vendor/js/bootstrap.min.js"
+        ]
+      }
     },
     stylesheets: {
       joinTo: "css/app.css",
