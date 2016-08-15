be228bd698905b1dccf72c835d1c768f8c796dc5
Jeff <200006506@qq.com>
2016-08-15 22:32:28 +0800

    修改 from_token

--- lib/phoenix_china/guardian.ex
+++ lib/phoenix_china/guardian.ex
@@ -32,9 +32,11 @@ defmodule PhoenixChina.GuardianSerializer do
   alias PhoenixChina.Repo
   alias PhoenixChina.User
 
+  require Ecto.Query
+
   def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
   def for_token(_), do: { :error, "Unknown resource type" }
 
-  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
+  def from_token("User:" <> id), do: { :ok, User |> Ecto.Query.preload([:github]) |> Repo.get(id) }
   def from_token(_), do: { :error, "Unknown resource type" }
 end
