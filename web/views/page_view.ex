3a4a56b7b94235e6c533daf7e57bfeb5f6bb03f5
Jeff <200006506@qq.com>
2016-10-12 17:43:57 +0800

    Add today tag

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -9,4 +9,10 @@ defmodule PhoenixChina.PageView do
 
      render("navs.html", conn: conn, navigation: navigation)
   end
+
+  def is_today?(datetime) do
+    datetime = PhoenixChina.ViewHelpers.strftime(datetime)
+    today = PhoenixChina.ViewHelpers.strftime(Timex.today)
+    datetime == today
+  end
 end
