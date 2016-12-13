a34c816c2b1eba28757127768b40bc39f1cd8ab2
Jeff <200006506@qq.com>
2016-12-13 17:04:05 +0800

    Review user_follow_controller

--- web/controllers/user_follow_controller.ex
+++ web/controllers/user_follow_controller.ex
@@ -2,6 +2,7 @@ defmodule PhoenixChina.UserFollowController do
   use PhoenixChina.Web, :controller
 
   alias PhoenixChina.{User, UserFollow, Notification}
+  alias Ecto.Multi
 
   import PhoenixChina.ViewHelpers, only: [current_user: 1]
   import PhoenixChina.Ecto.Helpers, only: [increment: 2, decrement: 2]
@@ -10,53 +11,53 @@ defmodule PhoenixChina.UserFollowController do
 
   def create(conn, %{"user_username" => username}) do
     current_user = current_user(conn)
-    to_user = User |> Repo.get_by!(username: username)
+    to_user = Repo.get_by!(User, username: username)
+    
+    multi =
+      Multi.new
+      |> Multi.insert(:user_follow, UserFollow.changeset(%UserFollow{}, %{user_id: current_user.id, to_user_id: to_user.id}))
+      |> Multi.update_all(:from_user, from(u in User, where: u.id == ^current_user.id), [inc: [followed_count: 1]])
+      |> Multi.update_all(:to_user, from(u in User, where: u.id == ^to_user.id), [inc: [follower_count: 1]])
+      |> Multi.run(:notification, fn _ -> 
+        notification_html = Notification.render "user_follow.html", conn: conn, user: current_user
+        Notification.publish "user_follow", to_user.id, current_user.id, current_user.id, notification_html
+        {:ok, nil}
+      end)
 
-    params = %{:user_id => current_user.id, :to_user_id => to_user.id}
-    changeset = UserFollow.changeset(%UserFollow{}, params)
-
-    case Repo.insert(changeset) do
-      {:ok, _user_follow} ->
-        to_user |> increment(:follower_count)
-        current_user |> increment(:followed_count)
-
-        notification_html = Notification.render "user_follow.html",
-          conn: conn,
-          user: current_user
-
-        Notification.publish(
-          "user_follow",
-          to_user.id,
-          current_user.id,
-          current_user.id,
-          notification_html
-        )
-
-        to_user |> increment(:unread_notifications_count)
+    case Repo.transaction(multi) do
+      {:ok, res} ->
+        IO.inspect res
 
         conn
         |> put_flash(:info, "关注成功.")
-        |> redirect(to: user_path(conn, :show, username))
-      {:error, _changeset} ->
+        
+      {:error, _} ->
         conn
-        |> put_flash(:info, "关注失败.")
-        |> redirect(to: user_path(conn, :show, username))
+        |> put_flash(:error, "关注失败.")
     end
+    |> redirect(to: user_path(conn, :show, username))
   end
 
   def delete(conn, %{"user_username" => username}) do
     current_user = current_user(conn)
-    to_user = User |> Repo.get_by!(username: username)
-
-    UserFollow
-    |> Repo.get_by!(user_id: current_user.id, to_user_id: to_user.id)
-    |> Repo.delete!
-
-    to_user |> decrement(:follower_count)
-    current_user |> decrement(:followed_count)
-
-    conn
-    |> put_flash(:info, "取消关注成功.")
+    to_user = Repo.get_by!(User, username: username)
+    user_follow = Repo.get_by!(UserFollow, user_id: current_user.id, to_user_id: to_user.id)
+    
+    multi = 
+      Multi.new
+      |> Multi.delete(:user_follow, user_follow)
+      |> Multi.update_all(:from_user, from(u in User, where: u.id == ^current_user.id), [inc: [followed_count: -1]])
+      |> Multi.update_all(:to_user, from(u in User, where: u.id == ^to_user.id), [inc: [follower_count: -1]])
+    
+    case Repo.transaction(multi) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "取消关注成功.")
+        
+      {:error, _} ->
+        conn
+        |> put_flash(:error, "取消关注失败.")
+    end
     |> redirect(to: user_path(conn, :show, username))
   end
 end
