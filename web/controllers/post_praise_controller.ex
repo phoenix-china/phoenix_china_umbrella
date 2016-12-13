e1f8796b17f3465dab219b1774772231658110de
Jeff <200006506@qq.com>
2016-12-13 18:14:31 +0800

    Review post_praise_controller

--- web/controllers/post_praise_controller.ex
+++ web/controllers/post_praise_controller.ex
@@ -2,6 +2,7 @@ defmodule PhoenixChina.PostPraiseController do
   use PhoenixChina.Web, :controller
 
   alias PhoenixChina.{Post, PostPraise, Notification}
+  alias Ecto.Multi
 
   import PhoenixChina.ViewHelpers, only: [current_user: 1]
   import PhoenixChina.Ecto.Helpers, only: [increment: 2, decrement: 2]
@@ -10,19 +11,22 @@ defmodule PhoenixChina.PostPraiseController do
 
   def create(conn, %{"post_id" => post_id}) do
     current_user = current_user(conn)
-    post = Post |> preload([:user]) |> Repo.get!(post_id)
-    params = %{:post_id => post_id, :user_id => current_user.id}
-    changeset = PostPraise.changeset(%PostPraise{}, params)
-
-    case Repo.insert(changeset) do
-      {:ok, post_praise} ->
-        post = increment(post, :praise_count)
-
+    
+    multi =
+      Multi.new
+      |> Multi.insert(:post_praise, PostPraise.changeset(%PostPraise{}, %{post_id: post_id, user_id: current_user.id}))
+      |> Multi.update_all(:post, from(p in Post, where: p.id == ^post_id), [inc: [praise_count: 1]])
+      |> Multi.run(:notification, fn %{post_praise: post_praise} -> 
         Notification.create(conn, post_praise)
+        {:ok, nil}
+      end)
 
+    case Repo.transaction(multi) do
+      {:ok, _} ->
         conn
-        |> render("show.json", post: post, is_praise: true)
-      {:error, _changeset} ->
+        |> render("show.json", post: Repo.get(Post, post_id), is_praise: true)
+
+      {:error, %{post_praise: changeset}} ->
         conn
         |> put_status(:bad_request)
         |> render("error.json", changeset: changeset)
@@ -31,18 +35,26 @@ defmodule PhoenixChina.PostPraiseController do
 
   def delete(conn, %{"post_id" => post_id}) do
     current_user = current_user(conn)
+    post_praise =  Repo.get_by(PostPraise, user_id: current_user.id, post_id: post_id)
+
+    multi =
+      Multi.new
+      |> Multi.delete(:post_praise, post_praise)
+      |> Multi.update_all(:post, from(p in Post, where: p.id == ^post_id), [inc: [praise_count: -1]])
+      |> Multi.run(:notification, fn _ -> 
+        Notification.delete(post_praise)
+        {:ok, nil}
+      end)
     
-    post = Post |> preload([:user]) |> Repo.get!(post_id)
+    case Repo.transaction(multi) do
+      {:ok, _} ->
+        conn
+        |> render("show.json", post: Repo.get(Post, post_id), is_praise: false)
 
-    post_praise = PostPraise |> Repo.get_by!(user_id: current_user.id, post_id: post_id)
-
-    Notification.delete(post_praise)
-
-    post_praise |> Repo.delete!
-
-    post = decrement(post, :praise_count)
-
-    conn
-    |> render("show.json", post: post, is_praise: false)
+      {:ok, _} ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{})
+    end
   end
 end
