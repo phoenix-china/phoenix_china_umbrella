53aa2b2f04fe859e91507535c0e48583ac1faf1f
Jeff <200006506@qq.com>
2016-12-26 15:27:13 +0800

    Update PhoenixChina.Guardian.CurrentUser

--- lib/guardian/current_user.ex
+++ lib/guardian/current_user.ex
@@ -6,20 +6,8 @@ defmodule PhoenixChina.Guardian.CurrentUser do
   def call(conn, opts) do
     key = Map.get(opts, :key, :default)
     
-    case Guardian.Plug.authenticated?(conn, key) do
-      true ->
-        current_user = Guardian.Plug.current_resource(conn, key)
-
-        conn
-        |> assign(:authenticated?, true)
-        |> assign(:current_user, current_user)
-
-      false ->
-        current_user = nil
-
-        conn
-        |> assign(:authenticated?, false)
-        |> assign(:current_user, current_user)
-    end
+    conn
+    |> assign(:authenticated?, Guardian.Plug.authenticated?(conn, key))
+    |> assign(:current_user, Guardian.Plug.current_resource(conn, key))
   end
 end
