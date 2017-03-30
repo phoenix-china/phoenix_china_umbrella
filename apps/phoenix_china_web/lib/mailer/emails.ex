5ba11f5a675a2ae6c7acef6d5d4613051404875f
Jeff <200006506@qq.com>
2017-03-30 17:39:37 +0800

    Fix ci error

--- apps/phoenix_china_web/lib/mailer/emails.ex
+++ apps/phoenix_china_web/lib/mailer/emails.ex
@@ -9,16 +9,22 @@ defmodule PhoenixChina.Emails do
   欢迎邮件
   """
   def welcome_email(person) do
-    base_email()
-    |> to(person)
-    |> subject("欢迎注册Phoenix中文社区")
-    |> render(:welcome)
-    |> Mailer.deliver_later
+      base_email()
+      |> to(person)
+      |> subject("欢迎注册Phoenix中文社区")
+      |> render(:welcome)
+      |> send
   end
 
   defp base_email do
     new_email()
     |> from(@from)
     |> put_html_layout({PhoenixChina.Web.LayoutView, "email.html"})
+  end
+
+  defp send(mail) do
+    if System.get_env("MAILGUN_API_KEY") && System.get_env("MAILGUN_DOMAIN") do
+      Mailer.deliver_later(mail)
+    end
   end
 end
--- apps/phoenix_china_web/lib/phoenix_china_web/controllers/page_controller.ex
+++ apps/phoenix_china_web/lib/phoenix_china_web/controllers/page_controller.ex
@@ -2,7 +2,6 @@ defmodule PhoenixChina.Web.PageController do
   use PhoenixChina.Web, :controller
 
   def index(conn, _params) do
-    IO.inspect System.get_env("MAILGUN_API_KEY")
     render conn, "index.html"
   end
 end
